7e60e080f6031693b98256557214d453f4a1db91
George Gruner <imgeorge220@gmail.com>
2020-01-15 11:31:38 -0800

    added pics and refactored js

--- tvmaze.js
+++ tvmaze.js
@@ -21,17 +21,18 @@ async function searchShows(query) {
   let response = await axios.get('http://api.tvmaze.com/search/shows', {params: {q: query}})
   console.log(response)
   let showList = [];
-  for (show in response.data) {
-  let image = '';
-  if (response.data[show].show.image) {
-    image = response.data[show].show.image.medium;
-  }
-   let newShow = {
-      name: response.data[show].show.name,
-      summary: response.data[show].show.summary,
-      image: image
-    }
-    showList.push(newShow);
+  for (let show in response.data) {
+    let defaultImg = 'https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300';
+    let { image, name, summary } = response.data[show].show;
+    
+    image = image ? image.medium : defaultImg;
+    
+    let newShow = {
+        name,
+        summary,
+        image
+      }
+      showList.push(newShow);
   }
   return showList;
 }
@@ -51,6 +52,7 @@ function populateShows(shows) {
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
            <div class="card-body">
+            <img class="card-img-top" src="${show.image}">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
            </div>
